3134ffbcb4260d9f012eadb9797e53595e26a6d1
jihadyaghi <jihadyaghie@gmail.com>
2025-11-22 22:36:27 +0200

    create navbar with SideBar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import {FaBars} from "react-icons/fa";
+import { FaTaxi } from 'react-icons/fa';
 
-function Navbar() {
+function Navbar({onSideBarHide}) {
   return (
-    <div>Navbar</div>
+    <nav className='flex justify-between items-center px-6 py-4 bg-black text-white' >
+      <div className='flex items-center gap-3'>
+        <button onClick={
+          ()=>{
+            onSideBarHide();
+          }
+        } >
+        <FaBars className='hover:text-yellow-400'/>
+      </button>
+      <h1 className='text-2xl font-bold  text-yellow-400 flex items-center gap-1'>
+        GO<FaTaxi className='inline-block '/> Taxi
+      </h1>
+</div>
+      
+      <div className='space-x-6 hidden md:flex' >
+        <Link to="/About" className='hover:text-yellow-400' >About</Link>
+        <Link to="/Contact" className='hover:text-yellow-400' >Contact</Link>
+
+      </div>
+
+    </nav>
   )
 }
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,26 @@
+import { useState } from 'react';
 import React from 'react'
+import Navbar from '../Components/Navbar'
+import Menu from '../Pages/Menu'
+
 
 function Home() {
+  const [isHidden, setIsHidden] = useState(false);
   return (
-    <div>Home</div>
+    <div>
+      <div>
+        <Navbar
+      onSideBarHide={() => {
+            setIsHidden(!isHidden);
+          }} 
+      />
+</div>
+    <div className={`col-span-1 ${isHidden ? "hidden" : "block"}`}>
+      <Menu/>
+
+</div>
+      
+    </div>
   )
 }
 
